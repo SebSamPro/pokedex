c646afed1f49f9f1de83a1abe086e29d27a6a6e0
SebSamPro <sebastien.samyn.pro@gmail.com>
2023-10-10 14:29:17 +0200

    réponse seb

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,8 @@
+function PokemonCard() {
+    <figure>
+      <img> src="/var/folders/x4/hvqdxpxd5xg_1w2y0stxmql00000gn/T/com.apple.useractivityd/shared-pasteboard/items/032A0504-F810-4388-869A-0E49BF5D9448/274dca6cba69980ad22e45b8122bc8407f2eae04.rtfd" atl="Bulbasaur"</img>
+      <figcaption>"Bulbasaur"</figcaption>
+    </figure>
+  }
+  
+  export default App
