fcf45cd8601809fb0d1c2a390eb6795701ec8995
SebSamPro <sebastien.samyn.pro@gmail.com>
2023-10-10 14:50:31 +0200

    solution finale pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import PokemonCard from "./components/PokemonCard"
+import PokemonCard from './components/PokemonCard'
 
-<>
-function App () {
+function App() {
   return (
     <div>
       <PokemonCard />
     </div>
-  );
+  )
 }
-</>
-export default App;
+
+export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,10 @@
 function PokemonCard() {
+  return (
     <figure>
-      <img> src="/var/folders/x4/hvqdxpxd5xg_1w2y0stxmql00000gn/T/com.apple.useractivityd/shared-pasteboard/items/032A0504-F810-4388-869A-0E49BF5D9448/274dca6cba69980ad22e45b8122bc8407f2eae04.rtfd" atl="Bulbasaur"</img>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
       <figcaption>"Bulbasaur"</figcaption>
     </figure>
-  }
+  )
+}
   
-  export default App
+export default PokemonCard;
