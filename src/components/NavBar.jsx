9d79a850eed7505a2c430cd75d146d7d440c1c80
SebSamPro <sebastien.samyn.pro@gmail.com>
2023-10-18 11:09:32 +0200

    Final Soluce quest 8 react

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,36 @@
-function NavBar ({nextClick, prevClick, pokemonIndex, pokemonList}) {
-    return ( 
+import { useState } from 'react'
+
+
+function NavBar({pokemonList, pokemonIndex, setpokemonIndex}){
+/*   const [pokemonIndex , setpokemonIndex] = useState(0); */
+console.log('pokelist',pokemonList);
+  const handleNextClick = () => {
+      setpokemonIndex(pokemonIndex +1)}
+  const handlePrevClick = () => {
+      setpokemonIndex(pokemonIndex -1)}
+   const handleClick =(index) => {setpokemonIndex(index)
+
+   }
+      
+    return(
         <>
-        <button disabled={pokemonIndex <= 0} onClick={prevClick}>precedent</button>
-        <button disabled= {pokemonIndex >= pokemonList.length -1} onClick={nextClick}>suivant</button>
-        </>
-     );
+ {/*    <button disabled={pokemonIndex <= 0} onClick={handlePrevClick}>precedent</button>
+        <button disabled= {pokemonIndex >= pokemonList.length -1} onClick={handleNextClick}>suivant</button> */}
+
+{
+  pokemonList.map((pokemon,index)=>(
+    <button key={pokemon.name} onClick={()=>handleClick(index)}>{pokemon.name}{index}</button>
+  ))
 }
 
-export default NavBar;
+
+
+        </>)
+        
+        
+        
+
+    
+}
+
+export default NavBar
